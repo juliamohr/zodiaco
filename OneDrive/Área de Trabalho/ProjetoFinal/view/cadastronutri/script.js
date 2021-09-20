e073f02867bf9d804ead00b5197430acf3684330
juliamohr <juliamcachoeira@gmail.com>
2021-09-20 10:33:02 -0300

    Finalização do zodiaco

--- OneDrive/Área de Trabalho/ProjetoFinal/view/cadastronutri/script.js
+++ OneDrive/Área de Trabalho/ProjetoFinal/view/cadastronutri/script.js
@@ -1,81 +1,66 @@
-//jQuery time
-var current_fs, next_fs, previous_fs; //fieldsets
-var left, opacity, scale; //fieldset properties which we will animate
-var animating; //flag to prevent quick multi-click glitches
 
-$(".next").click(function(){
-	if(animating) return false;
-	animating = true;
-	
-	current_fs = $(this).parent();
-	next_fs = $(this).parent().next();
-	
-	//activate next step on progressbar using the index of next_fs
-	$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-	
-	//show the next fieldset
-	next_fs.show(); 
-	//hide the current fieldset with style
-	current_fs.animate({opacity: 0}, {
-		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale current_fs down to 80%
-			scale = 1 - (1 - now) * 0.2;
-			//2. bring next_fs from the right(50%)
-			left = (now * 50)+"%";
-			//3. increase opacity of next_fs to 1 as it moves in
-			opacity = 1 - now;
-			current_fs.css({
-        'transform': 'scale('+scale+')',
-        'position': 'absolute'
-      });
-			next_fs.css({'left': left, 'opacity': opacity});
-		}, 
-		duration: 800, 
-		complete: function(){
-			current_fs.hide();
-			animating = false;
-		}, 
-		//this comes from the custom easing plugin
-		easing: 'easeInOutBack'
-	});
+function validateEmail(email) {
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
+    
+console.log(validateEmail('texto@texto.com')); // true
+console.log(validateEmail('texto@texto')); // false
+console.log(validateEmail('texto.com')); // false
+console.log(validateEmail('texto')); // false
+
+
+   var password = document.getElementById("password")
+   , confirm_password = document.getElementById("confirm_password");
+ 
+ function validatePassword(){
+   if(password.value != confirm_password.value) {
+     confirm_password.setCustomValidity("Senhas diferentes!");
+   } else {
+     confirm_password.setCustomValidity('');
+   }
+ }
+ 
+ password.onchange = validatePassword;
+ confirm_password.onkeyup = validatePassword;
+ $("#submit").on("click", function() {
+  if ($("#form-login").valid()) { //Verifica se o formulário está válido.
+    $('#myModal').modal('show'); //Se for válido, exibe o modal.
+  }
 });
-
-$(".previous").click(function(){
-	if(animating) return false;
-	animating = true;
-	
-	current_fs = $(this).parent();
-	previous_fs = $(this).parent().prev();
-	
-	//de-activate current step on progressbar
-	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-	
-	//show the previous fieldset
-	previous_fs.show(); 
-	//hide the current fieldset with style
-	current_fs.animate({opacity: 0}, {
-		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale previous_fs from 80% to 100%
-			scale = 0.8 + (1 - now) * 0.2;
-			//2. take current_fs to the right(50%) - from 0%
-			left = ((1-now) * 50)+"%";
-			//3. increase opacity of previous_fs to 1 as it moves in
-			opacity = 1 - now;
-			current_fs.css({'left': left});
-			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
-		}, 
-		duration: 800, 
-		complete: function(){
-			current_fs.hide();
-			animating = false;
-		}, 
-		//this comes from the custom easing plugin
-		easing: 'easeInOutBack'
-	});
-});
-
-$(".submit").click(function(){
-	return false;
+$('#myModal').on('shown.bs.modal', function () {
+  $('#meuInput').trigger('focus')
 })
+
+$('#myForm').on('submit', function(e) {
+  var email = $('#email');
+ 
+
+  // Check if there is an entered value
+  if(!email.val()) {
+    // Add errors highlight
+    email.closest('.form-group').removeClass('has-success').addClass('has-error');
+
+    // Stop submission of the form
+    e.preventDefault();
+  } else {
+    // Remove the errors highlight
+    email.closest('.form-group').removeClass('has-error').addClass('has-success');
+  }
+});
+$('#myForm').on('submit', function(e) {
+  var password = $('#password');
+ 
+
+  // Check if there is an entered value
+  if(!password.val()) {
+    // Add errors highlight
+    password.closest('.form-group').removeClass('has-success').addClass('has-error');
+
+    // Stop submission of the form
+    e.preventDefault();
+  } else {
+    // Remove the errors highlight
+    password.closest('.form-group').removeClass('has-error').addClass('has-success');
+  }
+});
